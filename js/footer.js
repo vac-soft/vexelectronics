90db0b25d3dedae76692cbbeb3231341021652ed
Kirubhakaran <57623979+dkkaran1996@users.noreply.github.com>
2023-03-21 14:50:52 +0530

    Revert "Update for navbar code reuse "

--- js/footer.js
+++
@@ -1,6 +0,0 @@
-const footer = document.querySelector('#footer');
-    fetch('./asset/footer.html')
-        .then(res => res.text())
-        .then(data => {
-            nav.innerHTML = data
-        })
